24944ea4376185820046917b8c4b2d972da66194
Jhansi108 <135030305+Jhansi108@users.noreply.github.com>
2023-11-22 18:06:39 +0530

    FactorialNumber

---
+++ FactorialNumber/ArrayEqual.java
@@ -0,0 +1,18 @@
+package FactrorialNum;
+
+import java.util.Arrays;
+
+public class ArrayEqual {
+
+	public static void main(String[] args) {
+		int[] array1 = new int[] {'a', 'b', 'c', 'd', 'e'};   
+		int[] array2 = new int[] {'a', 'b', 'c', 'd', 'e'};  
+		
+		if (Arrays.equals(array1, array2))   
+		System.out.println("Arrays are equal.");   
+		else  
+		System.out.println("Arrays are not equal."); 
+
+	}
+
+}
